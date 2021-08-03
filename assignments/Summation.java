d8d451fdec9969fc64fc45c17d555c6892bb42a0
Kranthi Guribilli <79071810+Kranthi-Guribilli@users.noreply.github.com>
2021-08-03 16:25:07 +0530

    Update and rename Summation. to Summation.java

---
+++ assignments/Summation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Summation{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter two numbers: ");
+    int n1=sc.nextInt();
+    int n2=sc.nextInt();
+    System.out.println(n1+n2+=+(n1+n2));
+  }
+}
