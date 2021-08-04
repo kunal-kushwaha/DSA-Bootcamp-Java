c84c56d75d8314b696521e834f97ab5c73f98e4e
BIKRAM-SAHA <bikramlearn15@gmail.com>
2021-08-04 21:06:24 +0530

    added <= to account for all perfect sq and removed 'if(n==4)' which is no more an edge case

--- lectures/5-first-java-program/first-idea-program/src/com/kunal/Prime.java
+++ lectures/5-first-java-program/first-idea-program/src/com/kunal/Prime.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
         System.out.print("Please enter a number: ");
         int n = in.nextInt();
+        int c = 2;
         if(n <= 1) {
             System.out.println("Neither prime nor composite");
             return;
         }
-        int c = 2;
-        if (n == 4) {
-            System.out.println("Not prime");
-        } else {
-            while (c * c < n) {
+        else {
+            while (c * c <= n) {
                 if (n % c == 0) {
                     System.out.println("Not Prime");
                     return;
@@ -26,6 +24,6 @@ public class Prime {
             if (c * c > n) {
                 System.out.println("Prime");
             }
-        }
+
     }
 }
