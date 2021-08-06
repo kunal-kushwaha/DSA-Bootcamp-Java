955adc4f0e5792341ed91d3eed86d8fd09d84518
Kranthi Guribilli <79071810+Kranthi-Guribilli@users.noreply.github.com>
2021-08-06 21:57:24 +0530

    Updated source code

--- assignments/Solutions-FirstJavaProgram/Arithmetic.java
+++ assignments/Solutions-FirstJavaProgram/Arithmetic.java
@@ -1,22 +1,44 @@
 import java.util.Scanner;
-public class Arithmetic{
-	public static void main(String[] args){
-		Scanner input=new Scanner(System.in);
-		System.out.print("Enter a number: ");
-		int num1=input.nextInt();
-		System.out.print("Enter an another number: ");
-		int num2=input.nextInt();
-		System.out.print("Enter an opertor(+,-,*,/):");
-		char op=input.next().charAt(0);
-		if(op=='+')
-			System.out.println(num1+"+"+num2+"= "+(num1+num2));
-		else if(op=='-')
-			System.out.println(num1+"-"+num2+"= "+(num1-num2));
-		else if(op=='*')
-			System.out.println(num1+"*"+num2+"= "+(num1*num2));
-		else if(op=='/')
-			System.out.println(num1+"/"+num2+"= "+(num1/num2));
-		else
-			System.out.println("Error: Enter a valid operator!!");
-	}
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Take input from user till user does not press X or x
+        int ans = 0;
+        while (true) {
+            // take the operator as input
+            System.out.print("Enter the operator: ");
+            char op = in.next().trim().charAt(0);
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                // input two numbers
+                System.out.print("Enter two numbers: ");
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid operation!!");
+            }
+            System.out.println(ans);
+        }
+    }
 }
