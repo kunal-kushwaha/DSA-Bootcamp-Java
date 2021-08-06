bd4d8e1021ec26e28fed440eb38671dfd3a4e83a
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2021-08-06 15:42:56 +0530

    l6

---
+++ lectures/6-conditions-loops/code/src/com/kunal/CaseCheck.java
@@ -0,0 +1,17 @@
+package com.kunal;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().trim().charAt(0);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        } else {
+            System.out.println("Uppercase");
+        }
+
+    }
+}
