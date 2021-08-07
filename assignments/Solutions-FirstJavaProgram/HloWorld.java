a4e8a497c764231e98bc9e53c7163d54a34cb5fd
Kranthi Guribilli <79071810+Kranthi-Guribilli@users.noreply.github.com>
2021-08-07 21:48:07 +0530

    Add files via upload

---
+++ assignments/Solutions-FirstJavaProgram/HloWorld.java
@@ -0,0 +1,5 @@
+public class HloWorld{
+    public static void main(String args[]){
+                System.out.println("Hello World!\nHey Kranthi, you run your first program successfully!!!");
+}
+}
