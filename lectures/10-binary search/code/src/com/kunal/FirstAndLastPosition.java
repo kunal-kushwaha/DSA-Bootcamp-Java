a6a9e4adafcbc0d274c180d49af27515eb178431
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2021-08-20 19:57:56 +0530

    Update FirstAndLastPosition.java
    Added a very very easy way for FirstAndLastPosition

--- lectures/10-binary search/code/src/com/kunal/FirstAndLastPosition.java
+++ lectures/10-binary search/code/src/com/kunal/FirstAndLastPosition.java
@@ -42,4 +42,55 @@ public class FirstAndLastPosition {
         }
         return ans;
     }
+    
+    
+    
+    ///   very very easy way 
+    
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] result = new int[2];
+
+        result[0] =FindingStartIndex(nums,target);
+        result[1] =FindingEndIndex(nums,target);
+         return result;
+    }
+
+    private int FindingEndIndex(int[] nums, int target) {
+        int index = -1 ;
+       int start = 0  ;
+        int end = nums.length -1  ;
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] >=target) end = mid -1 ;
+            else start = mid + 1 ;    // Here we're breaking out the loop
+             if (nums[mid] == target ) {  index  = mid ; }
+        }
+        return index ;
+    }
+
+    
+
+    private int FindingStartIndex(int[] nums, int target) {
+
+        int index = -1 ;
+        int start = 0  ;
+        int end = nums.length -1  ;
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] <= target ) {start = mid +1 ;}
+            else end = mid -1 ;
+            if (nums[mid] == target) index = mid ;
+        }
+        return index ;
+
+    }
+
+    
+    
+    
 }
