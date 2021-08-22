63c7649f9a65c5ee5d4f98f0adedc3f08e751687
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2021-08-22 18:36:17 +0530

    bubble

---
+++ lectures/11-sorting/code/src/com/kunal/Main.java
@@ -0,0 +1,35 @@
+package com.kunal;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr) {
+        boolean swapped;
+        // run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            // for each step, max item will come at the last respective index
+            for (int j = 1; j < arr.length - i; j++) {
+                // swap if the item is smaller than the previous item
+                if (arr[j] < arr[j-1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            // if you did not swap for a particular value of i, it means the array is sorted hence stop the program
+            if (!swapped) { // !false = true
+                break;
+            }
+        }
+    }
+}
