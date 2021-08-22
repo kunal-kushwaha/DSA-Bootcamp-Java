c917f3fff4d5ae3d4d9d8b332b6a3c0b54e08401
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-22 22:50:21 +0530

    bubble sort

--- lectures/10-binary search/code/src/com/kunal/adam/bubble.java
+++ lectures/10-binary search/code/src/com/kunal/adam/bubble.java
@@ -1,6 +1,30 @@
 package com.kunal.adam;
 public class bubble{
     public static void main(String[] args){
-        System.out.println("jehe");
+        
+        int[] arr={3,5,1,7,8};
+        bubblesort(arr);
     }
-}
+    static void bubblesort(int[] arr){
+        boolean x=false;
+               for(int i=0;i<arr.length;i++){
+                   for(int j=1;j<arr.length-i;j++){
+                       if(arr[j]<arr[j-1]){
+                           int temp=arr[j];
+                           arr[j]=arr[j-1];
+                           arr[j-1]=temp;
+                        x=true;
+                       }
+                      
+                   } if(!x){
+                    break;
+                }
+                   
+                }
+                for(int p=0;p<arr.length;p++){
+                    System.out.println(arr[p]);
+                }
+                
+               }
+               
+    }
