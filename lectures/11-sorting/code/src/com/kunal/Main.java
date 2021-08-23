d09fb80ca94d30c81df4e1dfb949cbf467fe14de
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2021-08-23 17:49:13 +0530

    insertion sort

--- lectures/11-sorting/code/src/com/kunal/Main.java
+++ lectures/11-sorting/code/src/com/kunal/Main.java
@@ -5,9 +5,21 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {4, 5, 1, 2, 3};
-        selection(arr);
+        int[] arr = {5, 3, 4, 1, 2};
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j, j-1);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     static void selection(int[] arr) {
