e19f9f8fa14fa889e07230c0fdc0f451ca8f14c0
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-23 21:18:03 +0530

    selection sort

--- lectures/10-binary search/code/src/com/kunal/adam/bubble.java
+++ lectures/10-binary search/code/src/com/kunal/adam/bubble.java
@@ -1,13 +1,17 @@
 package com.kunal.adam;
+
+import java.util.Arrays;
+
 public class bubble{
     public static void main(String[] args){
         
         int[] arr={3,5,1,7,8};
         bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
     }
     static void bubblesort(int[] arr){
-        boolean x=false;
-               for(int i=0;i<arr.length;i++){
+        
+               for(int i=0;i<arr.length;i++){boolean x=false;
                    for(int j=1;j<arr.length-i;j++){
                        if(arr[j]<arr[j-1]){
                            int temp=arr[j];
@@ -21,9 +25,7 @@ public class bubble{
                 }
                    
                 }
-                for(int p=0;p<arr.length;p++){
-                    System.out.println(arr[p]);
-                }
+                
                 
                }
                
---
+++ mycodes/select.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class select{
+    public static void main(String[] args){
+        int[] arr={4,2,7,3,1};
+int findmax=0;
+        for(int i=0;i<arr.length;i++){
+            int last = arr.length - i - 1;
+             findmax=maxfinder(arr,0,last);
+             swap(arr, findmax, last);
+        }System.out.println(Arrays.toString(arr));
+        
+    }
+    public static int maxfinder(int[] arr,int start,int end){
+       int max=start;
+       for(int j=0;j<=end;j++){
+           if(arr[j]>arr[max]){
+               max=j;
+           }
+       } return max;
+    }
+    public static void swap(int[] arr,int start,int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+    }
