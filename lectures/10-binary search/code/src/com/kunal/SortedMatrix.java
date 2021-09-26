af15cf8364092dda4697384d6f708b4858a767c1
ishaan gupta <52467684+ishaangupta-YB@users.noreply.github.com>
2021-09-27 00:34:21 +0530

    Update SortedMatrix.java

--- lectures/10-binary search/code/src/com/kunal/SortedMatrix.java
+++ lectures/10-binary search/code/src/com/kunal/SortedMatrix.java
@@ -1,82 +1,41 @@
-package com.kunal;
+package com.ishaan;
 
 import java.util.Arrays;
 
 public class SortedMatrix {
-    public static void main(String[] args) {
-        int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-        System.out.println(Arrays.toString(search(arr, 9)));
-    }
+	public static void main(String[] args) { 
+		int[][] a = {{10,25,28},
+                      {30,34},
+                      {40,50,53,59,90}
+                     }; 
+		
+		System.out.println(Arrays.toString(search(a, 59)));              
+	}
+	
+	static int[] search(int[][] a,int target) {
+		if(a.length == 0) return new int[] {-1,-1};        
+		
+		int rows = a.length;
+		int cols = a[0].length;		 // be cautious, matrix may be empty
 
-    // search in the row provided between the cols provided
-    static int[] binarySearch(int[][] matrix, int row, int cStart, int cEnd, int target) {
-        while (cStart <= cEnd) {
-            int mid = cStart + (cEnd - cStart) / 2;
-            if (matrix[row][mid] == target) {
-                return new int[]{row, mid};
+
+        for(int i=0;i<rows;i++) {
+        	cols=a[i].length;
+        	
+            if(a[i][cols-1] >= target && target >= a[i][0]) {       //	check whether the target is present in the current row.
+                                                                    //  if not then do move to next row (i++)
+            	int start=0,end=cols-1;
+
+                //  performing simple binary search
+            	while(start <= end) {
+            		int mid=start+(end-start)/2;
+
+            		if(a[i][mid] == target) return new int[] {i,mid};
+            		else if(a[i][mid] < target) start=mid+1;
+            		else end=mid-1;
+            	}
             }
-            if (matrix[row][mid] < target) {
-                cStart = mid + 1;
-            } else {
-                cEnd = mid - 1;
-            }
-        }
-        return new int[]{-1, -1};
-    }
-
-    static int[] search(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length; // be cautious, matrix may be empty
-        if (cols == 0){
-            return new int[] {-1,-1};
-        }
-        if (rows == 1) {
-            return binarySearch(matrix,0, 0, cols-1, target);
-        }
-
-        int rStart = 0;
-        int rEnd = rows - 1;
-        int cMid = cols / 2;
-
-        // run the loop till 2 rows are remaining
-        while (rStart < (rEnd - 1)) { // while this is true it will have more than 2 rows
-            int mid = rStart + (rEnd - rStart) / 2;
-            if (matrix[mid][cMid] == target) {
-                return new int[]{mid, cMid};
-            }
-            if (matrix[mid][cMid] < target) {
-                rStart = mid;
-            } else {
-                rEnd = mid;
-            }
-        }
-
-        // now we have two rows
-        // check whether the target is in the col of 2 rows
-        if (matrix[rStart][cMid] == target) {
-            return new int[]{rStart, cMid};
-        }
-        if (matrix[rStart + 1][cMid] == target) {
-            return new int[]{rStart + 1, cMid};
-        }
-
-        // search in 1st half
-        if (target <= matrix[rStart][cMid - 1]) {
-            return binarySearch(matrix, rStart, 0, cMid-1, target);
-        }
-        // search in 2nd half
-        if (target >= matrix[rStart][cMid + 1] && target <= matrix[rStart][cols - 1]) {
-            return binarySearch(matrix, rStart, cMid + 1, cols - 1, target);
-        }
-        // search in 3rd half
-        if (target <= matrix[rStart + 1][cMid - 1]) {
-            return binarySearch(matrix, rStart + 1, 0, cMid-1, target);
-        } else {
-            return binarySearch(matrix, rStart + 1, cMid + 1, cols - 1, target);
-        }
-    }
+        } 
+        return new int[] {-1,-1};			//	return if target not present in matrix
+	}
 }
