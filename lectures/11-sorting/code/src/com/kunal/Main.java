eef2cb44fbaf9ee675a73e6006d4d10ccf484784
Jason Dsouza <74404894+JasonDsouza212@users.noreply.github.com>
2021-10-09 13:57:06 +0530

    Update Main.java

--- lectures/11-sorting/code/src/com/kunal/Main.java
+++ lectures/11-sorting/code/src/com/kunal/Main.java
@@ -11,7 +11,7 @@ public class Main {
     }
 
     static void insertion(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) { // or we can also use for(int i=0;i<= arr.length-2;i++) 
             for (int j = i+1; j > 0; j--) {
                 if (arr[j] < arr[j-1]) {
                     swap(arr, j, j-1);
