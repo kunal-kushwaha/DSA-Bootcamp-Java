5c38694b511e431b11dda0089fc9666713f7a7b3
nik <77881638+nik132-eng@users.noreply.github.com>
2021-11-15 08:52:51 +0530

    add leetcode sollution
    Runtime: 5 ms, faster than 30.99% of Java online submissions for Letter Combinations of a Phone Number.
    Memory Usage: 39.2 MB, less than 53.08% of Java online submissions for Letter Combinations of a Phone Number.

--- lectures/14-recursion/code/src/com/kunal/strings/PhonePad.java
+++ lectures/14-recursion/code/src/com/kunal/strings/PhonePad.java
@@ -50,4 +50,62 @@ public class PhonePad {
         }
         return count;
     }
+    
+//     leetcode sollution solution ===========================================================================================
+//     https://leetcode.com/problems/letter-combinations-of-a-phone-number/submissions/
+//     =======================================================================================================================
+    
+	static ArrayList<String> letterCombinations_list_p_up(String p, String up) {
+		if (up.isEmpty()) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add(p);
+			return list;
+		}
+
+		ArrayList<String> list = new ArrayList<>();
+
+		// this will convert '2' into 2
+		int digit = up.charAt(0) - '0';
+
+		if (digit == 1) {
+			return null;
+		} else if (digit < 7) {
+
+			for (int i = (digit - 1) * 3; i < (digit * 3); i++) {
+				char ch = (char) ('a' + i - 3);
+				list.addAll(letterCombinations_list_p_up(p + ch, up.substring(1)));
+			}
+		} else {
+			if (digit == 7) {
+				for (int i = 7; i <= 10; i++) {
+					char ch = (char) (105 + i);
+					list.addAll(letterCombinations_list_p_up(p + ch, up.substring(1)));
+				}
+			} else if (digit == 8) {
+				for (int i = 8; i <= 10; i++) {
+					char ch = (char) (108 + i);
+					list.addAll(letterCombinations_list_p_up(p + ch, up.substring(1)));
+				}
+			} else {
+				for (int i = 9; i <= 12; i++) {
+					char ch = (char) (110 + i);
+					list.addAll(letterCombinations_list_p_up(p + ch, up.substring(1)));
+				}
+			}
+		}
+		return list;
+	}
+
+	static List<String> letterCombinations_list(String up) {
+		if (up.isEmpty()) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add("");
+			return list;
+		}
+		ArrayList<String> ans = new ArrayList<>();
+		ans = letterCombinations_list_p_up("",up);
+		return ans;
+
+	}
+
 }
