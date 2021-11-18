91be299696080a0931d5fa789e88fe4fe7d8ac9a
VedantPol <polvedant7@gmail.com>
2021-11-19 01:10:13 +0530

    unnecessary comment start

--- lectures/07-methods/code/src/com/kunal/Shadowing.java
+++ lectures/07-methods/code/src/com/kunal/Shadowing.java
@@ -5,7 +5,7 @@ public class Shadowing {
     public static void main(String[] args) {
         System.out.println(x); // 90
         int x; // the class variable at line 4 is shadowed by this
-//        System.out.println(x); // scope will begin when value is initialised
+//        System.out.println(x);  scope will begin when value is initialised
         x = 40;
         System.out.println(x); // 40
         fun();
