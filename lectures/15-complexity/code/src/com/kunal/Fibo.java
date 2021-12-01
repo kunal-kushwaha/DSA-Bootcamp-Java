a0482a78d33c70572b355cedd58a0b117476b04e
Deepa Chaurasiya <78865653+Deepa-chaurasiya01@users.noreply.github.com>
2021-12-01 20:09:31 +0530

    Update Fibo.java

--- lectures/15-complexity/code/src/com/kunal/Fibo.java
+++ lectures/15-complexity/code/src/com/kunal/Fibo.java
@@ -9,7 +9,7 @@ public class Fibo {
         System.out.println(fiboFormula(50));
     }
 
-    static long fiboFormula(int n){
+    static int fiboFormula(int n){
          // just for demo, use long instead
         return (int) ((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
     }
