8fb9f69c280b2aba175fbfe81c7984cf6579f007
abrehamgezahegn <gezahegnabereham@gmail.com>
2021-12-02 23:57:50 +0300

    feat: sorted matrix binart search shorter code with O(log n) complexity

--- lectures/10-binary search/code/src/com/kunal/SortedMatrix.java
+++ lectures/10-binary search/code/src/com/kunal/SortedMatrix.java
@@ -12,71 +12,43 @@ public class SortedMatrix {
         System.out.println(Arrays.toString(search(arr, 9)));
     }
 
-    // search in the row provided between the cols provided
-    static int[] binarySearch(int[][] matrix, int row, int cStart, int cEnd, int target) {
-        while (cStart <= cEnd) {
-            int mid = cStart + (cEnd - cStart) / 2;
-            if (matrix[row][mid] == target) {
-                return new int[]{row, mid};
-            }
-            if (matrix[row][mid] < target) {
-                cStart = mid + 1;
-            } else {
-                cEnd = mid - 1;
-            }
-        }
-        return new int[]{-1, -1};
-    }
 
     static int[] search(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length; // be cautious, matrix may be empty
-        if (cols == 0){
-            return new int[] {-1,-1};
-        }
-        if (rows == 1) {
-            return binarySearch(matrix,0, 0, cols-1, target);
-        }
+       int start = 0;
 
-        int rStart = 0;
-        int rEnd = rows - 1;
-        int cMid = cols / 2;
+        // we are going to treat the matrix like a regular array
+        // {
+        //   {1,2,3,4},
+        //   {5,6,7,8},
+        //   {9,10,11,12}
+        //  }
+        // end should point at 12
+        // and count starting from 1, 12 is at the 11th index
+        // therefore end = 11
+        // end = (3 - 1) * 4  + 3 = 8 + 3 = 11
+        int end  = (matrix.length - 1) * (matrix[0].length ) + (matrix[0].length - 1);
 
-        // run the loop till 2 rows are remaining
-        while (rStart < (rEnd - 1)) { // while this is true it will have more than 2 rows
-            int mid = rStart + (rEnd - rStart) / 2;
-            if (matrix[mid][cMid] == target) {
-                return new int[]{mid, cMid};
+        while(start <= end){
+            // 1st loop
+            // start = 0, end = 11, m = 5
+            int m = start + (end - start) / 2;
+
+            // convert back length to [i,j] to access the value
+            // e.g 5 => [5/3, 5%3] = [1, 2]
+            int i = m/matrix[0].length;
+            int j = m%matrix[0].length;
+
+            // apply BS
+            if(target == matrix[i][j]){
+                return new int[]{i,j};
             }
-            if (matrix[mid][cMid] < target) {
-                rStart = mid;
-            } else {
-                rEnd = mid;
+            if(target > matrix[i][j]){
+                start = m + 1;
+            }else {
+                end = m - 1;
             }
         }
 
-        // now we have two rows
-        // check whether the target is in the col of 2 rows
-        if (matrix[rStart][cMid] == target) {
-            return new int[]{rStart, cMid};
-        }
-        if (matrix[rStart + 1][cMid] == target) {
-            return new int[]{rStart + 1, cMid};
-        }
-
-        // search in 1st half
-        if (target <= matrix[rStart][cMid - 1]) {
-            return binarySearch(matrix, rStart, 0, cMid-1, target);
-        }
-        // search in 2nd half
-        if (target >= matrix[rStart][cMid + 1] && target <= matrix[rStart][cols - 1]) {
-            return binarySearch(matrix, rStart, cMid + 1, cols - 1, target);
-        }
-        // search in 3rd half
-        if (target <= matrix[rStart + 1][cMid - 1]) {
-            return binarySearch(matrix, rStart + 1, 0, cMid-1, target);
-        } else {
-            return binarySearch(matrix, rStart + 1, cMid + 1, cols - 1, target);
-        }
+        return new int[]{-1,-1};
     }
 }
