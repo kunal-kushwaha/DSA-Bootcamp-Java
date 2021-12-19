963ba027d43281f2d83a3e58f4e2cafc0c8afdf3
deep1910 <92329233+deep1910@users.noreply.github.com>
2021-12-19 22:31:02 +0530

    Update Shadowing.java

--- lectures/07-methods/code/src/com/kunal/Shadowing.java
+++ lectures/07-methods/code/src/com/kunal/Shadowing.java
@@ -4,9 +4,9 @@ public class Shadowing {
     static int x = 90; // this will be shadowed at line 8
     public static void main(String[] args) {
         System.out.println(x); // 90
-        int x; // the class variable at line 4 is shadowed by this
+        int x;//here variable  x is decalred // the class variable at line 4 is shadowed by this
 //        System.out.println(x); // scope will begin when value is initialised
-        x = 40;
+        x = 40;//here variable x is initialised
         System.out.println(x); // 40
         fun();
     }
