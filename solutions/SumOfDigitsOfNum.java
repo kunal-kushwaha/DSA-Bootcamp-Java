2ae7405284a00b4c90abd8c852a37fd05f464c75
P K Nayak <47595700+MekongDelta-mind@users.noreply.github.com>
2022-01-13 20:23:25 +0530

    Solution to the Question 25
    Adding the solution of Question 25 in the Intermediate Java Programs.

--- solutions/SumOfDigitsOfNum.java
+++ solutions/SumOfDigitsOfNum.java
@@ -1,1 +1,25 @@
+package org.dsawithkunal.conditionals;
 
+import java.util.Scanner;
+
+public class SumOfDigitsOfNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Enter the number for which you want to find the sum of digits \nNumber =");
+		Scanner in = new Scanner(System.in);
+		
+		int num = in.nextInt();
+		
+		int sum = 0 ;
+		int digit = 0;
+		while(num>0) {
+			digit = num%10;
+			sum = sum + digit;
+			num = num/10;
+		}
+		System.out.println("The Sum of each digits for the given number is " + sum);
+	}
+
+}
