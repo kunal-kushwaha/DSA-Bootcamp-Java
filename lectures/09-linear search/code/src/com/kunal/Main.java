f42abc26f5dc3d1f8e33be42d2c7c18bed23cc6a
Gokul Gajapathi <akgokul22@gmail.com>
2022-01-18 22:10:54 +0530

    One Return keyword reduced in every function

--- lectures/09-linear search/code/src/com/kunal/Main.java
+++ lectures/09-linear search/code/src/com/kunal/Main.java
@@ -3,18 +3,20 @@ package com.kunal;
 public class Main {
 
     public static void main(String[] args) {
-	    int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
-	    int target = 19;
-	    boolean ans = linearSearch3(nums, target);
+        int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+        int target = 19;
+        boolean ans = linearSearch3(nums, target);
         System.out.println(ans);
     }
 
     // search the target and return true or false
     static boolean linearSearch3(int[] arr, int target) {
         if (arr.length != 0) {
+            // run a for loop
             for (int element : arr) {
-            if (element == target) {
-                return true;
+                if (element == target) {
+                    return true;
+                }
             }
         }
         // this line will execute if none of the return statements above have executed
@@ -25,9 +27,11 @@ public class Main {
     // search the target and return the element
     static int linearSearch2(int[] arr, int target) {
         if (arr.length != 0) {
+            // run a for loop
             for (int element : arr) {
-            if (element == target) {
-                return element;
+                if (element == target) {
+                    return element;
+                }
             }
         }
         // this line will execute if none of the return statements above have executed
@@ -39,11 +43,13 @@ public class Main {
     // otherwise if item not found return -1
     static int linearSearch(int[] arr, int target) {
         if (arr.length != 0) {
+            // run a for loop
             for (int index = 0; index < arr.length; index++) {
-            // check for element at every index if it is = target
-            int element = arr[index];
-            if (element == target) {
-                return index;
+                // check for element at every index if it is = target
+                int element = arr[index];
+                if (element == target) {
+                    return index;
+                }
             }
         }
         // this line will execute if none of the return statements above have executed
