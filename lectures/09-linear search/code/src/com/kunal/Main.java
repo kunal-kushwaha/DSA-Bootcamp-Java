6969f9ac9122d9013bed7505b7bb6ec44f69b50c
Gokul Gajapathi <akgokul22@gmail.com>
2022-01-20 14:15:36 +0530

    changes added with comments

--- lectures/09-linear search/code/src/com/kunal/Main.java
+++ lectures/09-linear search/code/src/com/kunal/Main.java
@@ -5,13 +5,19 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
         int target = 19;
-        boolean ans = linearSearch3(nums, target);
-        System.out.println(ans);
+        boolean ans3 = linearSearch3(nums, target);
+        System.out.println("linearSearch3: "+ans3);
+        int ans2 = linearSearch2(nums, target);
+        System.out.println("linearSearch2: "+ans2);
+        int ans = linearSearch(nums, target);
+        System.out.println("linearSearch: "+ans);
     }
 
     // search the target and return true or false
     static boolean linearSearch3(int[] arr, int target) {
         if (arr.length != 0) {
+            /*    return false;
+        }*/ //if condition is not equal to zero it will run for loop
             // run a for loop
             for (int element : arr) {
                 if (element == target) {
@@ -26,14 +32,16 @@ public class Main {
 
     // search the target and return the element
     static int linearSearch2(int[] arr, int target) {
-        if (arr.length != 0) {
+        if (arr.length == 0) {
+            return -1;
+        }
             // run a for loop
             for (int element : arr) {
                 if (element == target) {
                     return element;
                 }
             }
-        }
+
         // this line will execute if none of the return statements above have executed
         // hence the target not found
         return Integer.MAX_VALUE;
@@ -43,6 +51,8 @@ public class Main {
     // otherwise if item not found return -1
     static int linearSearch(int[] arr, int target) {
         if (arr.length != 0) {
+            /*return -1;
+        }*/ //if condition is not equal to zero it will run for loop
             // run a for loop
             for (int index = 0; index < arr.length; index++) {
                 // check for element at every index if it is = target
