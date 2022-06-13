0dda6803277fe20b1e46ff5c3aba788da9b2eba4
Vi Hiếu BA10-019 <90641589+DeluxeJade@users.noreply.github.com>
2022-06-13 22:41:11 +0700

    update for testFindAllDupliacates

--- lectures/11-sorting/code/src/com/kunal/TestFindAllDuplicates.java
+++ lectures/11-sorting/code/src/com/kunal/TestFindAllDuplicates.java
@@ -1,11 +1,18 @@
 package com.kunal;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class TestFindAllDuplicates {
     @Test
     public void testFindAllDuplicates() {
-        assertEquals(1,FindAllDuplicates.findDuplicates(new int[] {1,1,1,2,3,4,5}));
+        List<Integer> expectedResult = new ArrayList<>();
+        expectedResult.add(3);
+        FindAllDuplicates findAllDupeInstance = new FindAllDuplicates();
+        assertEquals(expectedResult,findAllDupeInstance.findDuplicates(new int[]{1,2,3,4,5,3,6,7,8,9}));
     }
 }
