b3e141d6b3269ed6402710c83c06e8509ac6f7c1
Harshit Sahu <101046816+harshitsahu002@users.noreply.github.com>
2022-08-21 16:39:00 +0530

    Update FirstAndLastPosition.java

--- lectures/10-binary search/code/src/com/kunal/FirstAndLastPosition.java
+++ lectures/10-binary search/code/src/com/kunal/FirstAndLastPosition.java
@@ -2,6 +2,11 @@ package com.kunal;
 
 public class FirstAndLastPosition {
     public static void main(String[] args) {
+        int[] arr ={1,3,5,6,7,23,64,645,657};
+        int target = 64;
+        
+        int abs = searchRange(arr,target);
+        System.out.println(abs)
 
     }
 
