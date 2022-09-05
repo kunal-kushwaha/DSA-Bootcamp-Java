144b4ff1d08d1e6c9ebb73bf0389317668415bd0
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2022-09-05 17:16:11 +0530

    Create 2ed Answer.java

---
+++ assignments/2) first java/2ed Answer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SecondAnswer {
+    public static void main(String[] args) {
+        //Take name as input and print a greeting message for that particular name.
+        Scanner sc=new Scanner(System.in);
+        String Name = sc.nextLine();// Taking input from user
+        System.out.println("Have a nice day "+Name);
+
+    }
+}
