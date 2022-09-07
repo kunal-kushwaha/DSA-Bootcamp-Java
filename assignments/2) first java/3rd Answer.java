9cd78ac82a84877724913f8cfe2ea379fa2e2823
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2022-09-07 21:48:47 +0530

    New File Added

--- assignments/2) first java/3rd Answer.java
+++ assignments/2) first java/3rd Answer.java
@@ -4,9 +4,13 @@ public class thirdAnswers {
     public static void main(String[] args) {
         //Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter three numbers");
         int principal  = sc.nextInt();
+        System.out.println();
         int time = sc.nextInt();
+        System.out.println();
         int rate= sc.nextInt();
+        System.out.println();
 
         System.out.println("Simple Interest:- "+ principal*rate*time);
     }
