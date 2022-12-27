3ad902a2d59c40adedea5ed9fcde7111fd68d3cb
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-27 23:39:06 +0530

    change in for loop

--- lectures/11-sorting/code/src/com/kunal/Main.java
+++ lectures/11-sorting/code/src/com/kunal/Main.java
@@ -50,7 +50,8 @@ public class Main {
     static void bubble(int[] arr) {
         boolean swapped;
         // run the steps n-1 times
-        for (int i = 0; i < arr.length; i++) {
+        
+        for (int i = 0; i < arr.length-1; i++) {
             swapped = false;
             // for each step, max item will come at the last respective index
             for (int j = 1; j < arr.length - i; j++) {
