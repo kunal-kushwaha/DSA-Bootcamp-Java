eda8f7849f06430bc71df8d9d1135f560d2e2fda
Sujeeth Varma <122418068+Sujeethvarma27@users.noreply.github.com>
2023-02-18 17:24:11 +0530

    Initialized an Array to Main Function

--- lectures/09-linear search/code/src/com/kunal/MaxWealth.java
+++ lectures/09-linear search/code/src/com/kunal/MaxWealth.java
@@ -2,6 +2,12 @@ package com.kunal;
 // https://leetcode.com/problems/richest-customer-wealth/
 public class MaxWealth {
     public static void main(String[] args) {
+        int[][] accounts = {
+                {1,2,3},
+                {3,2,1},
+                {2,5,1},
+        };
+        System.out.println(maximumWealth(accounts));
 
     }
     public int maximumWealth(int[][] accounts) {
