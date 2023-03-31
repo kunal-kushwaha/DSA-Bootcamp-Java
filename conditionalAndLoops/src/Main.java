619a7909496b87361e862eb51e40a701b07786d4
Sahil Ansari Mondal <sahilansarimondal@gmail.com>
2023-03-31 21:45:38 +0530

    15 Questions solved

---
+++ conditionalAndLoops/src/Main.java
@@ -0,0 +1,247 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+
+//        Write Java programs for the following:
+
+
+//        Basic Java Programs
+
+
+//        1,Area Of Circle Java Program
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter radius to get area of circle :");
+        int r = in.nextInt();
+
+        float p = r*r*3.1416f;
+        System.out.println("The area is : " + p);
+
+         */
+
+//        2,Area Of Triangle
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the base value :");
+        float b = in.nextInt();
+        System.out.println("Enter the height value :");
+        float h = in.nextInt();
+
+        float area = b * h * 0.5f;
+
+        System.out.println("The area of Triangle is :" + area);
+
+         */
+
+//        3,Area Of Rectangle Program
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter height of rectangle :");
+        int h = in.nextInt();
+        System.out.println("Enter width of rectangle :");
+        int w = in.nextInt();
+
+        int area = h*w;
+
+        System.out.println("Area of rectangle is :" + area);
+
+         */
+
+//        4,Area Of Isosceles Triangle
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter height :");
+        int h = in.nextInt();
+        System.out.println("Enter width :");
+        int w = in.nextInt();
+
+        float area = (h*w)/2f;
+        System.out.println("Area is :" + area);
+
+         */
+
+//        5,Area Of Parallelogram
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Height :");
+        int h = in.nextInt();
+        System.out.println("Enter Base :");
+        int b = in.nextInt();
+
+        int area = b * h ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        5,Area Of Rhombus
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter d1 :");
+        int d1 = in.nextInt();
+        System.out.println("Enter d2 :");
+        int d2 = in.nextInt();
+
+        float area = (d1 * d2)/2f ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        6,Area Of Equilateral Triangle
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter side value :");
+        int d1 = in.nextInt();
+        float sqr = (float) Math.sqrt(3);
+
+        float area = (d1 * d1 * sqr)/4f ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        7,Perimeter Of Circle
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter radius :");
+        int r = in.nextInt();
+
+        float area = 2*r*3.1416f ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        8,Perimeter Of Equilateral Triangle
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Side :");
+        int s = in.nextInt();
+
+        float area = s*3f ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        9,Perimeter Of Parallelogram
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Side :");
+        int s = in.nextInt();
+        System.out.println("Enter Base :");
+        int b = in.nextInt();
+
+        int area = 2*(s+b) ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        10,Perimeter Of Rectangle
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter length :");
+        int l = in.nextInt();
+        System.out.println("Enter width :");
+        int w = in.nextInt();
+
+        int area = 2*(l+w) ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        11,Perimeter Of Square
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Side :");
+        int s = in.nextInt();
+
+        int area = 4*s ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        12,Perimeter Of Rhombus
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Side :");
+        int s = in.nextInt();
+
+        int area = 4*s ;
+
+        System.out.println("Total area is :" + area);
+
+         */
+
+//        13,Volume Of Cone Java Program
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Radius :");
+        float r = in.nextFloat();
+        System.out.println("Enter Float :");
+        float h = in.nextFloat();
+
+        float ans = (float) ((h/3)*(3.1416*r*r));
+        System.out.println("Ans :" + ans);
+
+         */
+
+//        14,Volume Of Prism
+
+
+
+//        15,Volume Of Cylinder
+
+        //____________________________------------------------------------------___________________________________
+
+
+//        16,Volume Of Sphere
+//        17,Volume Of Pyramid
+//        18,Curved Surface Area Of Cylinder
+//        19,Total Surface Area Of Cube
+//        20,Fibonacci Series In Java Programs
+//        21,Subtract the Product and Sum of Digits of an Integer
+//        22,Input a number and print all the factors of that number (use loops).
+//        23,Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+//        24,Take integer inputs till the user enters 0 and print the largest number from all.
+//        25,Addition Of Two Numbers
+
+
+//        26,Intermediate Java Programs
+//
+
+//        27,Factorial Program In Java
+//        28,Calculate Electricity Bill
+//        29,Calculate Average Of N Numbers
+//        30,Calculate Discount Of Product
+//        31,Calculate Distance Between Two Points
+//        32Calculate Commission Percentage
+//        Power In Java
+//        Calculate Depreciation of Value
+//        Calculate Batting Average
+//        Calculate CGPA Java Program
+//        Compound Interest Java Program
+//        Calculate Average Marks
+//        Sum Of N Numbers
+//        Armstrong Number In Java
+//        Find Ncr & Npr
+//        Reverse A String In Java
+//        Find if a number is palindrome or not
+//        Future Investment Value
+//        HCF Of Two Numbers Program
+//        LCM Of Two Numbers
+//        Java Program Vowel Or Consonant
+//        Perfect Number In Java
+//        Check Leap Year Or Not
+//        Sum Of A Digits Of Number
+//        Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
+//        Write a program to print the sum of negative numbers, sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. The list terminates when the user enters a zero.
+
+    }
+}
