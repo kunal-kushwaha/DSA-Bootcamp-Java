63bfaa6d07c86e85979f841395e8c03fcc3cf9e1
igokhul <gokhul.tg@proton.me>
2023-04-30 18:18:45 +0530

    changes

--- SolvedProblems/Recursion/StringSubset.java
+++ SolvedProblems/Recursion/StringSubset.java
@@ -1,13 +1,13 @@
 package SolvedProblems.Recursion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StringSubset {
-    static ArrayList<String> list = new ArrayList<>();
     static void subset(String processed, String unprocessed){
         
         if(unprocessed.isEmpty()){
-            list.add(processed);
+            System.out.print(processed + " ");
             return ;
         }
 
@@ -17,12 +17,62 @@ public class StringSubset {
         // Leave it
         subset(processed, unprocessed.substring(1));
     }
+
+    static ArrayList<String> subsetRet(String processed, String unprocessed){
+        
+        if(unprocessed.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        // Take it
+        ArrayList<String> left = subsetRet(processed + unprocessed.charAt(0), unprocessed.substring(1));
+
+        // Leave it
+        ArrayList<String> right = subsetRet(processed, unprocessed.substring(1));
+
+        left.addAll(right);
+
+        return left;
+    }
+
+    // Converting the recursion into iteration
+    static List<List<Integer>> subsetIteration(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        
+        outer.add(new ArrayList<>());
+
+        for(int num : arr){
+            int size = outer.size();
+            for (int i = 0; i < size; i++) {
+                ArrayList<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+
     public static void main(String[] args) {
-        String str = "abc";
+        // String str = "abc";
 
-        subset("", str);
+        // // subset("", str);
 
-        for(int i = 0; i < list.size(); i++)
-            System.out.print(list.get(i) + " ");
+        // ArrayList<String> list = subsetRet("", str);
+
+        // for(int i = 0; i < list.size(); i++){
+        //     System.out.print(list.get(i) + " ");
+        // }
+
+        // For recursion into iteration
+        int[] arr = {1,2,3};
+        List<List<Integer>> ans = subsetIteration(arr);
+
+        for (List<Integer> list : ans){
+            System.out.println(list);
+        }
+
     }
 }
