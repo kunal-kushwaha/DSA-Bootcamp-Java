c3fe18eb1d7c576a53c134613ea1e6e7309ef33b
PriyankaSep04 <52813133+PriyankaSep04@users.noreply.github.com>
2023-05-20 14:01:31 +0530

    Update PowOfTwo.java
    Fixed for n=0

--- lectures/16-math/code/src/com/kunal/bitwise/PowOfTwo.java
+++ lectures/16-math/code/src/com/kunal/bitwise/PowOfTwo.java
@@ -3,6 +3,9 @@ package com.kunal.bitwise;
 public class PowOfTwo {
     public static void main(String[] args) {
         int n = 31; // note: fix for n = 0
+        if(n==0){
+            System.out.println(false);
+        }
         boolean ans = (n & (n-1)) == 0;
         System.out.println(ans);
     }
