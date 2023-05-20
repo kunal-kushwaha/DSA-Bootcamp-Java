f7aafc06553d45076662b53f774b3c419a8f2205
igokhul <gokhul.tg@proton.me>
2023-05-20 21:35:12 +0530

    xcv

--- SolvedProblems/SomeConcepts/RoughWork.java
+++ SolvedProblems/SomeConcepts/RoughWork.java
@@ -1,28 +1,18 @@
 package SolvedProblems.SomeConcepts;
 
-import java.util.Arrays;
-
 public class RoughWork {
-    public static int[] cyclicSort(int[] arr) {
-        for (int i = 0; i < arr.length; ) {
-            int checkIndex = arr[i] - 1; 
-            if(arr[i] != arr[checkIndex]){
-                int temp = arr[i];
-                arr[i] = arr[checkIndex];
-                arr[checkIndex] = temp;
-            }
-            else{
-                i++;
-            }
-        }
-
-        return arr;
+    public static void main(String[] args) {
+        RoughWork obj = new RoughWork();
+        obj.fun1();
     }
 
-    public static void main(String[] args) {
-        int[] arr = { 9, 1, 8, 2, 3, 7, 6, 4, 5 };
-        arr = cyclicSort(arr);
-        System.out.println("Cyclic sort: " + Arrays.toString(arr));
+    void fun1(){
+        System.out.println("Hello");
+        fun2();
+    }
+
+    void fun2(){
+        System.out.println("World");
     }
 }
 
