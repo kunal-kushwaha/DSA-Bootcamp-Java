15d6c17e2236875edbe270aeb62b5ac18b22ffff
Yash Garg <62419230+mr-mods-yg@users.noreply.github.com>
2023-07-03 11:56:58 +0530

    explained the code in comments.
    Completed my work to fix the issue.

--- lectures/10-binary search/code/src/com/kunal/Ceiling.java
+++ lectures/10-binary search/code/src/com/kunal/Ceiling.java
@@ -19,16 +19,28 @@ public class Ceiling {
         int end = arr.length - 1;
         while(start < end) {
             // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
             
-            if (target <= arr[mid]) {
+            if (target < arr[mid]) {
+                end = mid+1;
+            }else if(target==arr[mid]){
                 // ans found
-                //but there might be same answer before it.
-                //so instead of return mid
-                //we will check the array again from start to mid (included)
-                end = mid;
+                // but there might be same answer before it.
+                // Suppose we have an array :
+                //    -> index : 0 1 2 3 4 5 6 7
+                // int[] nums = {3,3,6,6,7,8,8,9};
+                // ---------------s----m------e
+                // According to previous code, it will return index 3, but it is wrong in this case.
+                // ans should be index 2 as this is first smallest element greator than or equal to target.
+                // so instead of return mid
+                // we will check the array again from start to mid (included)
+                end=mid;
+                // condition for infinite loop -> end will be equal to mid 
+                // but it will be possible only when start will be equal to end
+                // that will be our while loop condition
+
             } else{
                 start = mid + 1;
             }
