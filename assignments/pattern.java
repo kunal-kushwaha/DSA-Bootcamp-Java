a16c5ba20e0a086fcf8285da617d491e478bcfaa
Shaily Tripathi <123561590+shaz12t@users.noreply.github.com>
2023-07-22 15:52:43 +0530

    Create pattern.java
    work on the new idea

---
+++ assignments/pattern.java
@@ -0,0 +1,9 @@
+# Define the number of rows and columns for the pattern
+rows = 5
+columns = 5
+
+# Loop to print the pattern
+for i in range(rows):
+    for j in range(columns):
+        print("*", end="")
+    print()
