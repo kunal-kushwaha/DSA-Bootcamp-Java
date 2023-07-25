f86a8d9da5fa4c69c052028609bb4b4387799cc4
Prashant Kumar Patel <70816283+itspraskpatel@users.noreply.github.com>
2023-07-25 15:03:06 +0530

    Update StaticBlock.java
    Fixed a typo

--- lectures/17-oop/code/src/com/kunal/staticExample/StaticBlock.java
+++ lectures/17-oop/code/src/com/kunal/staticExample/StaticBlock.java
@@ -5,7 +5,7 @@ public class StaticBlock {
     static int a = 4;
     static int b;
 
-    // will only run once, when the first obj is create i.e. when the class is loaded for the first time
+    // will only run once, when the first obj is created i.e. when the class is loaded for the first time
     static {
         System.out.println("I am in static block");
         b = a * 5;
