2ebe87643164e2c3a198024e3efbd3bae7ff79e2
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2023-07-29 10:43:34 +0530

    Update the code to find the height
    Earlier the code was giving height as 3 for tree with 1000, which is wrong as the answer should be 9 ( Floor of log(1000) base 2 will be 9 )

--- lectures/20-trees/code/AVL/AVL.java
+++ lectures/20-trees/code/AVL/AVL.java
@@ -1,162 +1,162 @@
-class AVL {
+package com.company;
 
-  public class Node {
-    private int value;
-    private Node left;
-    private Node right;
-    private int height;
+public class AVL {
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
 
-    public Node(int value) {
-      this.value = value;
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
-    public int getValue() {
-      return value;
-    }
-  }
+    private Node root;
 
-  private Node root;
+    public AVL() {
 
-  public AVL() {
-
-  }
-
-  public int height() {
-    return height(root);
-  }
-  private int height(Node node) {
-    if (node == null) {
-      return -1;
-    }
-    return node.height;
-  }
-  
-  public void insert(int value) {
-    root = insert(value, root);
-  }
-
-  private Node insert(int value, Node node) {
-    if (node == null) {
-      node = new Node(value);
-      return node;
     }
 
-    if (value < node.value) {
-      node.left = insert(value, node.left);
+    public int height() {
+        return height(root);
+    }
+    private int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
     }
 
-    if (value > node.value) {
-      node.right = insert(value, node.right);
+    public void insert(int value) {
+        root = insert(value, root);
     }
 
-    node.height = Math.max(height(node.left), height(node.right)) + 1;
-    return rotate(node);
-  }
+    private Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
 
-  private Node rotate(Node node) {
-    if (height(node.left) - height(node.right) > 1) {
-      // left heavy
-      if(height(node.left.left) - height(node.left.right) > 0) {
-        // left left case
-        return rightRotate(node);
-      }
-      if(height(node.left.left) - height(node.left.right) < 0) {
-        // left right case
-        node.left = leftRotate(node.left);
-        return rightRotate(node);
-      }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = height(node);
+        return rotate(node);
     }
 
-    if (height(node.left) - height(node.right) < -1) {
-      // right heavy
-      if(height(node.right.left) - height(node.right.right) < 0) {
-        // right right case
-        return leftRotate(node);
-      }
-      if(height(node.right.left) - height(node.right.right) > 0) {
-        // left right case
-        node.right = rightRotate(node.right);
-        return leftRotate(node);
-      }
+    private Node rotate(Node node) {
+        if (height(node.left) - height(node.right) > 1) {
+            // left heavy
+            if(height(node.left.left) - height(node.left.right) > 0) {
+                // left left case
+                return rightRotate(node);
+            }
+            if(height(node.left.left) - height(node.left.right) < 0) {
+                // left right case
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+
+        if (height(node.left) - height(node.right) < -1) {
+            // right heavy
+            if(height(node.right.left) - height(node.right.right) < 0) {
+                // right right case
+                return leftRotate(node);
+            }
+            if(height(node.right.left) - height(node.right.right) > 0) {
+                // left right case
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
+
+        return node;
     }
 
-    return node;
-  }
+    public Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
 
-  public Node rightRotate(Node p) {
-    Node c = p.left;
-    Node t = c.right;
+        c.right = p;
+        p.left = t;
 
-    c.right = p;
-    p.left = t;
-    
-    p.height = Math.max(height(p.left), height(p.right) + 1);
-    c.height = Math.max(height(c.left), height(c.right) + 1);
+        p.height = height(p);
+        c.height = height(c);
 
-    return c;
-  }
-
-  public Node leftRotate(Node c) {
-    Node p = c.right;
-    Node t = p.left;
-
-    p.left = c;
-    c.right = t;
-    
-    p.height = Math.max(height(p.left), height(p.right) + 1);
-    c.height = Math.max(height(c.left), height(c.right) + 1);
-
-    return p;
-  }
-
-  public void populate(int[] nums) {
-    for (int i = 0; i < nums.length; i++) {
-      this.insert(nums[i]);
-    }
-  }
-
-  public void populatedSorted(int[] nums) {
-    populatedSorted(nums, 0, nums.length);
-  }
-
-  private void populatedSorted(int[] nums, int start, int end) {
-    if (start >= end) {
-      return;
+        return c;
     }
 
-    int mid = (start + end) / 2;
+    public Node leftRotate(Node c) {
+        Node p = c.right;
+        Node t = p.left;
 
-    this.insert(nums[mid]);
-    populatedSorted(nums, start, mid);
-    populatedSorted(nums, mid + 1, end);
-  }
+        p.left = c;
+        c.right = t;
 
-  public void display() {
-    display(this.root, "Root Node: ");
-  }
+        p.height = height(p);
+        c.height = height(c);
 
-  private void display(Node node, String details) {
-    if (node == null) {
-      return;
+        return p;
     }
-    System.out.println(details + node.value);
-    display(node.left, "Left child of " + node.value + " : ");
-    display(node.right, "Right child of " + node.value + " : ");
-  }
 
-  public boolean isEmpty() {
-    return root == null;
-  }
-
-  public boolean balanced() {
-    return balanced(root);
-  }
-
-  private boolean balanced(Node node) {
-    if (node == null) {
-      return true;
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
     }
-    return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
-  }
 
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
+    }
+
+    private void populatedSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
+    }
+
+    public void display() {
+        display(this.root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left, "Left child of " + node.value + " : ");
+        display(node.right, "Right child of " + node.value + " : ");
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
 }
