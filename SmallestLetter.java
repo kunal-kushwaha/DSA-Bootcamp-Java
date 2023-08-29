f257a876d8482419667f4a3109e6e661231784cd
sahil kumar <kumarsahil7753@gmail.com>
2023-08-29 21:56:11 +0530

    Recreating changes from fix-feature-branch

---
+++ SmallestLetter.java
@@ -0,0 +1,27 @@
+package com.kunal;
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+public class SmallestLetter {
+
+    public static void main(String[] args) {
+
+    }
+
+    public char nextGreatestLetter(char[] letters, char target) {
+
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
---
+++ SplitArray.java
@@ -0,0 +1,45 @@
+package com.kunal;
+// https://leetcode.com/problems/split-array-largest-sum/
+public class SplitArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int splitArray(int[] nums, int m) {
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            start = Math.max(start, nums[i]); // in the end of the loop this will contain the max item of the array
+            end += nums[i];
+        }
+
+        // binary search
+        while (start < end) {
+            // try for the middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    // say you add this num in new subarray, then sum = num
+                    sum = num;
+                    pieces++;
+                } else {
+                    sum += num;
+                }
+            }
+
+            if (pieces > m) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+
+        }
+        return end; // here start == end
+    }
+}
