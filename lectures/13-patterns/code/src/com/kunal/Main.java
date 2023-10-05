07d5aac8680761790225122a33fb01f60d09ffaa
Aayush Sharma <aayush.sh24@outlook.com>
2023-10-05 18:00:46 +0530

    Updated Main.java
    proposed update should give the required pattern but it works well till 10.
    If possible please let me know what I'm doing wrong here

--- lectures/13-patterns/code/src/com/kunal/Main.java
+++ lectures/13-patterns/code/src/com/kunal/Main.java
@@ -10,7 +10,7 @@ public class Main {
         n = 2 * n;
         for (int row = 0; row <= n; row++) {
             for (int col = 0; col <= n; col++) {
-                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row, n - col));
+                int atEveryIndex = originalN - Math.min(Math.min(row, col), Math.min(n - row -2, n - col -2));
                 System.out.print(atEveryIndex + " ");
             }
             System.out.println();
