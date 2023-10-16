6418b2e148c44df334c7f1b454e77c518e3d9a7a
sanjay binnar <72009615+Binnar81@users.noreply.github.com>
2023-10-16 21:04:01 +0530

    explaination of each code line added in comment format

--- lectures/18-linkedlist/code/src/com/kunal/LL.java
+++ lectures/18-linkedlist/code/src/com/kunal/LL.java
@@ -2,138 +2,138 @@ package com.kunal;
 
 public class LL {
 
-    private Node head;
-    private Node tail;
-    private int size;
+    private Node head; // Reference to the first node in the linked list
+    private Node tail; // Reference to the last node in the linked list
+    private int size; // Keeps track of the number of nodes in the linked list
 
     public LL() {
-        this.size = 0;
+        this.size = 0; // Constructor to initialize the size to 0
     }
 
     public void insertFirst(int val) {
-        Node node = new Node(val);
-        node.next = head;
-        head = node;
+        Node node = new Node(val); // Create a new node with the specified value
+        node.next = head; // Set the new node's next reference to the current head
+        head = node; // Update the head to point to the new node
 
         if (tail == null) {
-            tail = head;
+            tail = head; // If this is the first node, also update the tail
         }
-        size += 1;
+        size += 1; // Increase the size of the linked list
     }
 
     public void insertLast(int val) {
         if (tail == null) {
-            insertFirst(val);
+            insertFirst(val); // If the list is empty, insert as the first node
             return;
         }
-        Node node = new Node(val);
-        tail.next = node;
-        tail = node;
-        size++;
+        Node node = new Node(val); // Create a new node with the specified value
+        tail.next = node; // Set the current tail's next reference to the new node
+        tail = node; // Update the tail to point to the new node
+        size++; // Increase the size of the linked list
     }
 
     public void insert(int val, int index) {
         if (index == 0) {
-            insertFirst(val);
+            insertFirst(val); // If the index is 0, insert at the beginning
             return;
         }
         if (index == size) {
-            insertLast(val);
+            insertLast(val); // If the index is the size, insert at the end
             return;
         }
 
         Node temp = head;
         for (int i = 1; i < index; i++) {
-            temp = temp.next;
+            temp = temp.next; // Traverse the list to find the node before the target index
         }
 
-        Node node = new Node(val, temp.next);
-        temp.next = node;
-
-        size++;
+        Node node = new Node(val, temp.next); // Create a new node and adjust references
+        temp.next = node; // Update the references to insert the new node
+        size++; // Increase the size of the linked list
     }
 
     // insert using recursion
     public void insertRec(int val, int index) {
-        head = insertRec(val, index, head);
+        head = insertRec(val, index, head); // Call the recursive insert method and update the head
     }
+
+    // Private recursive insert method
     private Node insertRec(int val, int index, Node node) {
         if (index == 0) {
-            Node temp = new Node(val, node);
+            Node temp = new Node(val, node); // Base case: Insert a new node at the specified index
             size++;
             return temp;
         }
 
-        node.next = insertRec(val, index-1, node.next);
+        node.next = insertRec(val, index - 1, node.next); // Recursively call the method
         return node;
     }
 
-
     public int deleteLast() {
         if (size <= 1) {
-            return deleteFirst();
+            return deleteFirst(); // If there's only one node, delete the first
         }
 
-        Node secondLast = get(size - 2);
-        int val = tail.value;
-        tail = secondLast;
-        tail.next = null;
-        size--;
-        return val;
+        Node secondLast = get(size - 2); // Find the second-to-last node
+        int val = tail.value; // Get the value of the last node
+        tail = secondLast; // Update the tail
+        tail.next = null; // Remove the reference to the old last node
+        size--; // Decrease the size of the linked list
+        return val; // Return the value of the deleted node
     }
 
     public int delete(int index) {
         if (index == 0) {
-            return deleteFirst();
+            return deleteFirst(); // If the index is 0, delete the first node
         }
         if (index == size - 1) {
-            return deleteLast();
+            return deleteLast(); // If the index is the size - 1, delete the last node
         }
 
-        Node prev = get(index - 1);
-        int val = prev.next.value;
+        Node prev = get(index - 1); // Find the node before the target index
+        int val = prev.next.value; // Get the value of the node to be deleted
 
-        prev.next = prev.next.next;
-        size--;
-        return val;
+        prev.next = prev.next.next; // Adjust references to remove the node
+        size--; // Decrease the size of the linked list
+        return val; // Return the value of the deleted node
     }
 
     public Node find(int value) {
         Node node = head;
         while (node != null) {
             if (node.value == value) {
-                return node;
+                return node; // Find and return the node with the specified value
             }
-            node = node.next;
+            node = node.next; // Move to the next node
         }
-        return null;
+        return null; // Value not found in the linked list
     }
 
     public Node get(int index) {
         Node node = head;
         for (int i = 0; i < index; i++) {
-            node = node.next;
+            node = node.next; // Traverse the list to find the node at the specified index
         }
-        return node;
+        return node; // Return the node at the specified index
     }
 
     public int deleteFirst() {
-        int val = head.value;
-        head = head.next;
+        int val = head.value; // Get the value of the first node
+        head = head.next; // Update the head to the next node
         if (head == null) {
-            tail = null;
+            tail = null; // If the list is now empty, update the tail
         }
-        size--;
-        return val;
+        size--; // Decrease the size of the linked list
+        return val; // Return the value of the deleted node
     }
 
     public void display() {
         Node temp = head;
         while (temp != null) {
-            System.out.print(temp.value + " -> ");
-            temp = temp.next;
+            System.out.print(temp.value + " -> "); // Print the value of each node
+            temp = temp.next; // Move to the next node
         }
-        System.out.println("END");
+        System.out.println("END"); // Print "END" to mark the end of the list
     }
 
     private class Node {
@@ -141,11 +141,11 @@ public class LL {
         private Node next;
 
         public Node(int value) {
-            this.value = value;
+            this.value = value; // Constructor for creating a node with a value
         }
 
         public Node(int value, Node next) {
-            this.value = value;
+            this.value = value; // Constructor for creating a node with a value and a next node reference
             this.next = next;
         }
     }
@@ -156,14 +156,14 @@ public class LL {
 
         while (node.next != null) {
             if (node.value == node.next.value) {
-                node.next = node.next.next;
-                size--;
+                node.next = node.next.next; // Remove duplicate nodes
+                size--; // Decrease the size of the linked list
             } else {
-                node = node.next;
+                node = node.next; // Move to the next node
             }
         }
-        tail = node;
-        tail.next = null;
+        tail = node; // Update the tail
+        tail.next = null; // Remove the reference to the old last duplicate
     }
 
     // https://leetcode.com/problems/merge-two-sorted-lists/submissions/
@@ -171,38 +171,38 @@ public class LL {
         Node f = first.head;
         Node s = second.head;
 
-        LL ans = new LL();
+        LL ans = new LL(); // Create a new linked list for the merged result
 
         while (f != null && s != null) {
             if (f.value < s.value) {
-                ans.insertLast(f.value);
-                f = f.next;
+                ans.insertLast(f.value); // Insert the smaller value from the first list
+                f = f.next; // Move to the next node in the first list
             } else {
-                ans.insertLast(s.value);
-                s = s.next;
+                ans.insertLast(s.value); // Insert the smaller value from the second list
+                s = s.next; // Move to the next node in the second list
             }
         }
 
         while (f != null) {
-            ans.insertLast(f.value);
-            f = f.next;
+            ans.insertLast(f.value); // Insert remaining nodes from the first list
+            f = f.next; // Move to the next node in the first list
         }
 
         while (s != null) {
-            ans.insertLast(s.value);
-            s = s.next;
+            ans.insertLast(s.value); // Insert remaining nodes from the second list
+            s = s.next; // Move to the next node in the second list
         }
 
-        return ans;
+        return ans; // Return the merged linked list
     }
 
     public void bubbleSort() {
-        bubbleSort(size - 1, 0);
+        bubbleSort(size - 1, 0); // Start the bubble sort algorithm
     }
 
     private void bubbleSort(int row, int col) {
         if (row == 0) {
-            return;
+            return; // If only one row left, the list is sorted
         }
 
         if (col < row) {
@@ -212,45 +212,45 @@ public class LL {
             if (first.value > second.value) {
                 // swap
                 if (first == head) {
-                    head = second;
-                    first.next = second.next;
+                    head = second; // Update the head
+                    first.next = second.next; // Adjust references for the swap
                     second.next = first;
                 } else if (second == tail) {
                     Node prev = get(col - 1);
-                    prev.next = second;
-                    tail = first;
-                    first.next = null;
+                    prev.next = second; // Update references for the swap
+                    tail = first; // Update the tail
+                    first.next = null; // Remove the reference to the old last node
                     second.next = tail;
                 } else {
                     Node prev = get(col - 1);
-                    prev.next = second;
-                    first.next = second.next;
+                    prev.next = second; // Update references for the swap
+                    first.next = second.next; // Adjust references for the swap
                     second.next = first;
                 }
             }
-            bubbleSort(row, col + 1);
+            bubbleSort(row, col + 1); // Recursively continue the sorting
         } else {
-            bubbleSort(row - 1, 0);
+            bubbleSort(row - 1, 0); // Move to the next row
         }
     }
 
-    // recursion reverse
+    // Recursively reverse the linked list
     private void reverse(Node node) {
         if (node == tail) {
-            head = tail;
+            head = tail; // Update the head
             return;
         }
-        reverse(node.next);
-        tail.next = node;
-        tail = node;
-        tail.next = null;
+        reverse(node.next); // Recursively reverse the list
+        tail.next = node; // Update references for the reverse
+        tail = node; // Update the tail
+        tail.next = null; // Remove the reference to the old last node
     }
 
-    // in place reversal of linked list
-    // google, microsoft, apple, amazon: https://leetcode.com/problems/reverse-linked-list/
+    // In-place reversal of linked list
+    // LeetCode problem: https://leetcode.com/problems/reverse-linked-list/
     public void reverse() {
         if (size < 2) {
-            return;
+            return; // If there are 0 or 1 elements, no need to reverse
         }
 
         Node prev = null;
@@ -258,16 +258,15 @@ public class LL {
         Node next = present.next;
 
         while (present != null) {
-            present.next = prev;
+            present.next = prev; // Reverse the references
             prev = present;
             present = next;
             if (next != null) {
                 next = next.next;
             }
         }
-        head = prev;
+        head = prev; // Update the head to the new first node
     }
-
 
     public static void main(String[] args) {
         LL first = new LL();
@@ -282,19 +281,15 @@ public class LL {
         second.insertLast(9);
         second.insertLast(14);
 
-        LL ans = LL.merge(first, second);
-        ans.display();
+        LL ans = LL.merge(first, second); // Merge two sorted lists
+        ans.display(); // Display the merged list
 
         LL list = new LL();
         for (int i = 7; i > 0; i--) {
-            list.insertLast(i);
+            list.insertLast(i); // Insert values into a new list
         }
-        list.display();
-        list.bubbleSort();
-        list.display();
-
+        list.display(); // Display the unsorted list
+        list.bubbleSort(); // Sort the list using bubble sort
+        list.display(); // Display the sorted list
     }
-
-
-
 }
