8d256b21c56d9ee97ea48a21818e141d57b08fcb
vedant kasar <vedantvvk5@gmail.com>
2024-01-24 21:29:11 +0530

    enhanced sudoku solver

--- lectures/14-recursion/code/src/com/kunal/backtracking/SudokuSolver.java
+++ lectures/14-recursion/code/src/com/kunal/backtracking/SudokuSolver.java
@@ -19,62 +19,43 @@ public class SudokuSolver {
         } else {
             System.out.println("Cannot solve");
         }
-
     }
 
     static boolean solve(int[][] board) {
         int n = board.length;
-        int row = -1;
-        int col = -1;
 
-        boolean emptyLeft = true;
-
-        // this is how we are replacing the r,c from arguments
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 0) {
-                    row = i;
-                    col = j;
-                    emptyLeft = false;
-                    break;
-                }
-            }
-            // if you found some empty element in row, then break
-            if (emptyLeft == false) {
-                break;
-            }
-        }
-
-        if (emptyLeft == true) {
-            return true;
-            // soduko is solved
-        }
-
-        // backtrack
-        for (int number = 1; number <= 9; number++) {
-            if (isSafe(board, row, col, number)) {
-                board[row][col] = number;
-                if (solve(board)) {
-                    // found the answer
-                    return true;
-                } else {
-                    // backtrack
-                    board[row][col] = 0;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == 0) {
+                    for (int number = 1; number <= 9; number++) {
+                        if (isSafe(board, row, col, number)) {
+                            board[row][col] = number;
+                            if (solve(board)) {
+                                // found the answer
+                                return true;
+                            } else {
+                                // backtrack
+                                board[row][col] = 0;
+                            }
+                        }
+                    }
+                    // no valid number found, backtrack to the previous cell
+                    return false;
                 }
             }
         }
-        return false;
+        // all cells are filled, the Sudoku is solved
+        return true;
     }
 
     private static void display(int[][] board) {
-        for(int[] row : board) {
-            for(int num : row) {
+        for (int[] row : board) {
+            for (int num : row) {
                 System.out.print(num + " ");
             }
             System.out.println();
         }
     }
-
 
     static boolean isSafe(int[][] board, int row, int col, int num) {
         // check the row
@@ -85,6 +66,7 @@ public class SudokuSolver {
             }
         }
 
+        
         // check the col
         for (int[] nums : board) {
             // check if the number is in the col
@@ -93,7 +75,7 @@ public class SudokuSolver {
             }
         }
 
-        int sqrt = (int)(Math.sqrt(board.length));
+        int sqrt = (int) (Math.sqrt(board.length));
         int rowStart = row - row % sqrt;
         int colStart = col - col % sqrt;
 
@@ -107,3 +89,4 @@ public class SudokuSolver {
         return true;
     }
 }
+
