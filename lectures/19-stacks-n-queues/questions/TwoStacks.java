bfbce0f7288d03e8342565cb65e2b6f3e7fd0820
Naveen John <65065036+naveenjohn942-prog@users.noreply.github.com>
2024-02-09 19:49:39 +0530

    Update TwoStacks.java
    linne 18 it should be b[0]

--- lectures/19-stacks-n-queues/questions/TwoStacks.java
+++ lectures/19-stacks-n-queues/questions/TwoStacks.java
@@ -15,7 +15,7 @@ class TwoStacks {
     }
 
     int ans1 = twoStacks(x, Arrays.copyOfRange(a, 1, a.length), b, sum + a[0], count + 1);
-    int ans2 = twoStacks(x, a, Arrays.copyOfRange(b, 1, b.length), sum + a[0], count + 1);
+    int ans2 = twoStacks(x, a, Arrays.copyOfRange(b, 1, b.length), sum + b[0], count + 1);
 
     return Math.max(ans1, ans2);
   }
